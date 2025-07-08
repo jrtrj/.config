de5ed050bfc372d4ed01ad26b1eff71a2cf5d528
jrtrj <jerit.reji.dev@gmail.com>
2025-07-09 01:28:30 +0530

    commit

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,9 @@
+vim.cmd.colorscheme "kanagawa"
+vim.opt.number = true       -- Show line numbers
+vim.o.mouse = ""
+vim.opt.tabstop = 4         -- Tab appears as 4 spaces
+vim.opt.shiftwidth = 4      -- Indent uses 4 spaces
+vim.opt.expandtab = true    -- Convert tabs to spaces
+vim.opt.softtabstop = 4     -- Treat 4 spaces as a tab (for backspace)
+vim.opt.wrap = false
+vim.opt.relativenumber = true -- Show relative line numbers
---
+++ nvim/lua/config/plugins/vim_be_good.lua
@@ -0,0 +1,3 @@
+return {
+    { "ThePrimeagen/vim-be-good", name = "vim-be-good", priority = 1000 }
+}
